18958595452a900170eae2c7f0fc0458c0e8a692
sebatian <juan_sforeroc@soy.sena.edu.co>
2025-04-23 14:16:59 -0500

    Admi

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Body from './components/Body'
-import Administrador from './components/Administrador'
+
 import MiCuenta from './components/MiCuenta'
 import Empleados from './components/Empleados/Empleados.jsx'
 import Cliente from './components/Cliente/Cliente.jsx'
@@ -37,7 +37,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Body />} />
         <Route path="/cuenta" element={<MiCuenta />} />
-        <Route path="/administrador" element={<Administrador />} />
+        
         <Route path="/cliente" element={<Cliente />} />
         <Route path="/empleados" element={<Empleados />} />
         <Route path="/home" element={<Body />} />
